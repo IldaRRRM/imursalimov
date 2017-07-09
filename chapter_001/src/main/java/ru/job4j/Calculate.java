ee790e7ec7f58a4736b3b6affb601ae45b61aa79
IldaRRRM <Ildarrr91@mail.ru>
2017-07-09 21:48:18 +0300

    Task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j;
-
+/**
+*Class Calculate - решение задачи части 001. урок 3
+*@author imursalimov
+*@Since  09.07.2017
+*/
 public class Calculate {
+	
+	/**
+	*class main - конструктор, вывод строки в консоль
+	*@param args - args
+	*/
 	public static void main (String[] args){
 		System.out.println("Hello, world");
 	}
