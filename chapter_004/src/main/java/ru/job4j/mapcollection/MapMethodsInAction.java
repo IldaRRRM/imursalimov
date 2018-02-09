5cb06acf27636078824e1344836c938ab04027ac
IldaRRRM <Ildarrr91@mail.ru>
2018-02-09 10:36:18 +0300

    1. Создать модель User[#999]

---
+++ chapter_004/src/main/java/ru/job4j/mapcollection/MapMethodsInAction.java
@@ -0,0 +1,4 @@
+package ru.job4j.mapcollection;
+
+public class MapMethodsInAction {
+}
---
+++ chapter_004/src/main/java/ru/job4j/mapcollection/User.java
@@ -0,0 +1,53 @@
+package ru.job4j.mapcollection;
+
+import java.util.Calendar;
+/**
+ * Model User.
+ */
+public class User {
+
+    private final String name;
+
+    private  int children;
+
+    private Calendar birthday = new Calendar.Builder().build();
+
+    /**
+     * Constructor.
+     * @param name - name of User.
+     * @param children - amount of children.
+     * @param year - year.
+     * @param  month - month.
+     * @param date - date.
+     */
+    public User(String name, int children, int year, int month, int date) {
+        this.name = name;
+        this.children = children;
+        this.birthday.set(year, month - 1, date);
+
+    }
+
+    /**
+     *Getter
+     * @return - return name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Return amount of children.
+     * @return - amount of children.
+     */
+    public int getChildren() {
+        return children;
+    }
+
+    /**
+     * Getter.
+     * @return - Birthday date.
+     */
+    public Calendar getBirthday() {
+        return birthday;
+    }
+}
