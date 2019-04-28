508e0917d9aab2d6d8b9193e56ae222eda8020dd
IldaRRRM <ildarrr91@mail.ru>
2019-04-28 12:35:16 +0300

    1. Читаем файл конфигурации [ #858] Исправления к задаче

--- chapter_006/src/test/java/ru/job4j/config/ConfigTest.java
+++ chapter_006/src/test/java/ru/job4j/config/ConfigTest.java
@@ -9,12 +9,13 @@ public class ConfigTest {
 
     @Test
     public void testStringOutPut() throws InterruptedException {
-        Config config = new Config("/home/dar/Desktop/projects/imursalimov/app.properties");
+        Config config = new Config(ClassLoader.getSystemResource("app.properties").getPath());
         config.load();
         Assert.assertThat(config.value("hibernate.connection.url"), is("jdbc:postgresql://127.0.0.1:5432/trackstudio"));
         Assert.assertThat(config.value("hibernate.dialect"), is("org.hibernate.dialect.PostgreSQLDialect"));
         Assert.assertThat(config.value("hibernate.connection.driver_class"), is("org.postgresql.Driver"));
         Assert.assertThat(config.value("hibernate.connection.username"), is("postgres"));
+        Assert.assertThat(config.value("hibernate.connection.password"), is("password"));
     }
 
 
